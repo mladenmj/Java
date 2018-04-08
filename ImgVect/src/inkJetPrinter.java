250fb2d5ce1b610be9cc93e2d26b6aabde446f20
Mladen <33460160+mladenmj@users.noreply.github.com>
2018-04-08 23:09:17 +0200

    inizio stampante

--- ImgVect/src/inkJetPrinter.java
+++ ImgVect/src/inkJetPrinter.java
@@ -1,3 +1,12 @@
 public class inkJetPrinter {
+    private final int MAXLEVEL;
+    private int[] cartridge;
 
+    public inkJetPrinter() {
+        MAXLEVEL = 100;
+        this.cartridge = new int[Colore.values().length];
+        //initcartridge;
+    }
+
+    
 }
