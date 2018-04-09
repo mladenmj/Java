75214ce91a7078bc7ce66bc20f70e6e11514937a
Mladen <33460160+mladenmj@users.noreply.github.com>
2018-04-09 11:00:20 +0200

    Stampante

--- ImgVect/src/inkJetPrinter.java
+++ ImgVect/src/inkJetPrinter.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class inkJetPrinter {
     private final int MAXLEVEL;
     private int[] cartridge;
@@ -5,8 +7,47 @@ public class inkJetPrinter {
     public inkJetPrinter() {
         MAXLEVEL = 100;
         this.cartridge = new int[Colore.values().length];
-        //initcartridge;
+        initCartridge();
     }
 
-    
+    public void initCartridge() {
+        for (Colore c : Colore.values()) {
+            this.cartridge[c.ordinal()] = MAXLEVEL;
+        }
+    }
+
+    public void changeCartridge(Colore c) {
+        cartridge[c.ordinal()] = MAXLEVEL;
+    }
+
+    public boolean checkCrtrdgLvl(ImgVect i) {
+        for(Colore c : Colore.values()) {
+            if(i.AreaColore(c) > this.cartridge[c.ordinal()]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void stampa(ImgVect i) {
+        if(checkCrtrdgLvl(i)) {
+            for (Colore c : Colore.values()) {
+                cartridge[c.ordinal()] -= i.AreaColore(c);
+            }
+
+            System.out.println("Stampa avvenuta con successo.\n");
+        }
+
+        else
+            System.out.println("Livello di inchiostro non sufficiente per stampare tutti gli elementi.\n");
+    }
+
+    public void showCrtrdgLevel() {
+        System.out.println("Ink level:");
+
+        for (Colore c : Colore.values()) {
+            System.out.println(c.name() + " cartridge: " + cartridge[c.ordinal()]);
+        }
+    }
 }
--- ImgVect/src/testPrinter.java
+++ ImgVect/src/testPrinter.java
@@ -1,5 +1,23 @@
 public class testPrinter {
     public static void main(String[] args) {
-        System.out.println("Ok.");
+        inkJetPrinter HP = new inkJetPrinter();
+        ImgVect i = new ImgVect(6);
+
+        Forma f = new Quadrato(Colore.RED, 9);
+        Forma f2 = new Quadrato(Colore.YELLOW, 5);
+        Forma f3 = new Rettangolo(Colore.BLACK, 8, 3);
+        Forma f5 = new Rettangolo(Colore.RED, 5, 6);
+        Cerchio f4 = new Cerchio(Colore.BLUE, 8);
+
+        i.aggForma(f);
+        i.aggForma(f2);
+        i.aggForma(f3);
+        i.aggForma(f4);
+        i.aggForma(f5);
+
+        HP.initCartridge();
+        HP.checkCrtrdgLvl(i);
+        HP.stampa(i);
+        HP.showCrtrdgLevel();
     }
 }
